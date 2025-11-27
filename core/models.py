6988f67002b5a4778f5ff6cba3b4893a5e10f181
beatrizritter <beatrizritter@gmail.com>
2025-11-27 14:43:47 -0300

    ok (fiz até parte de engenharia química)

--- core/models.py
+++ core/models.py
@@ -3,26 +3,27 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 
-
 User = get_user_model()
 
+
+class Disciplina(models.Model):
+    nome = models.CharField(max_length=100, unique=True)
+    codigo = models.CharField(max_length=10, unique=True, blank=True, null=True)
+    
+    def __str__(self):
+        return f'{self.nome} ({self.codigo or "Sem Código"})'
+
+
 class Avaliacao(models.Model):
-
-
     usuario = models.ForeignKey(User, on_delete=models.CASCADE) 
     
-
-    disciplina = models.CharField(max_length=100)
-    professor = models.CharField(max_length=100)
+    disciplina = models.ForeignKey(Disciplina, on_delete=models.CASCADE)
     
-
+    professor = models.CharField(max_length=100) 
+    
     nota = models.DecimalField(max_digits=2, decimal_places=1) 
-    
     comentario = models.TextField()
-    
-
     data_criacao = models.DateTimeField(auto_now_add=True) 
 
     def __str__(self):
-
-        return f'{self.disciplina} ({self.professor}) - Nota: {self.nota}'
+        return f'{self.disciplina.nome} ({self.professor}) - Nota: {self.nota}'
